e825be5ffea4459bfc427913660a43c7dca7a6f6
Daniel Waechtler <daniel.waechtler@student.uni-halle.de>
2022-10-23 18:12:52 +0200

    update gitignore

--- server/target/debug/deps/server-1057f4c0ad0a343b.d
+++
@@ -1,7 +0,0 @@
-/home/kami/Documents/Coding/Rust/GroupChat/server/target/debug/deps/server-1057f4c0ad0a343b.rmeta: src/main.rs
-
-/home/kami/Documents/Coding/Rust/GroupChat/server/target/debug/deps/server-1057f4c0ad0a343b.d: src/main.rs
-
-src/main.rs:
-
-# env-dep:CLIPPY_ARGS=
--- server/target/debug/deps/server-57f5671a9916ae1a.d
+++
@@ -1,7 +0,0 @@
-/home/kami/Documents/Coding/Rust/GroupChat/server/target/debug/deps/server-57f5671a9916ae1a.rmeta: src/main.rs
-
-/home/kami/Documents/Coding/Rust/GroupChat/server/target/debug/deps/server-57f5671a9916ae1a.d: src/main.rs
-
-src/main.rs:
-
-# env-dep:CLIPPY_ARGS=
